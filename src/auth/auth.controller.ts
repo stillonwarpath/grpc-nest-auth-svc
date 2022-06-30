78f727de4c89bd139f4b9149a84ace2d472dd828
francisco <fr.betancourt@live.com>
2022-06-30 16:50:13 -0400

    Added gRPC method handlers in auth controller.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject } from '@nestjs/common';
+import { GrpcMethod } from '@nestjs/microservices';
+import { AuthService } from './service/auth.service';
+import { AUTH_SERVICE_NAME, RegisterResponse, LoginResponse, ValidateResponse } from './auth.pb';
+import { RegisterRequestDto, LoginRequestDto, ValidateRequestDto } from './auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    @Inject(AuthService)
+    private readonly service: AuthService;
+
+    @GrpcMethod(AUTH_SERVICE_NAME, 'Register')
+    private register(payload: RegisterRequestDto): Promise<RegisterResponse> {
+        return this.service.login(payload);
+    }
+
+    @GrpcMethod(AUTH_SERVICE_NAME, 'Login')
+    private login(payload: LoginRequestDto): Promise<LoginResponse> {
+        return this.service.login(payload);
+    }
+    
+    @GrpcMethod(AUTH_SERVICE_NAME, 'Validate')
+    private validate(payload: ValidateRequestDto): Promise<ValidateResponse> {
+        return this.service.validate(payload);
+    }
+}
